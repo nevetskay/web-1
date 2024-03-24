db97d47ff0cdfc449f8c5f37c9eeb50c0dabff58
lebronkin <woodendx@yandex.ru>
2024-03-24 17:12:20 +0300

    Simple form

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require ("path")
+const fs = require("fs")
+const app = express()
+const port = 3030
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static(__dirname))
+app.get('/reg', (req, res) =>{
+    res.sendFile("reg_form.html", {root: path.join(__dirname, )})
+})
+app.get('/reg', (req, res) =>{
+    res.sendFile("style.css", {root: path.join(__dirname, )})
+})
+app.get('/reg', (req, res) =>{
+    res.sendFile("script.js", {root: path.join(__dirname, )})
+})
+app.post('/reg-data', (req, res) =>{
+    console.log(">>>>>>>>>>>>>>", req.body)
+    fs.appendFile("data.txt", JSON.stringify(req.body) + "/n", (err)=>{
+        if(err){
+            res.status(500).send("User not added")
+        } else{
+            res.status(201).send("User added")
+        }
+    })
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
